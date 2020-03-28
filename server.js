4125466f6c7038eed628bfef5e623da8db431b20
Oleksandr Nepsha <oleksandr.nepsha@cashstar.com>
2020-03-28 17:41:54 +0200

    Dirty integration of mongodb

--- server.js
+++ server.js
@@ -1,87 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-
-let testData = [
-    {
-        id: 1,
-        date: '2012-04-23T18:25:43.511Z',
-        content: '',
-        title: 'Some title here 312895',
-        excerpt: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores, perspiciatis?'
-    },
-
-    {
-        id: 2,
-        date: '2012-04-23T18:25:43.511Z',
-        content: '',
-        title: 'Some title here 2',
-        excerpt: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores, perspiciatis?'
-    },
-    {
-        id: 3,
-        date: '2014-04-23T18:25:43.511Z',
-        content: '',
-        title: 'Some title here 3',
-        excerpt: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores, perspiciatis?'
-    },
-    {
-        id: 4,
-        date: '2012-04-23T18:25:43.511Z',
-        content: '',
-        title: 'Some title here 4',
-        excerpt: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores, perspiciatis?'
-    },
-    {
-        id: 5,
-        date: '2017-04-23T18:25:43.511Z',
-        content: '',
-        title: 'Some title here 5',
-        excerpt: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores, perspiciatis?'
-    },
-    {
-        id: 6,
-        date: '2012-04-23T18:25:43.511Z',
-        content: '',
-        title: 'Some title here 6',
-        excerpt: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores, perspiciatis?'
-    },
-    {
-        id: 7,
-        date: '2012-04-23T18:25:43.511Z',
-        content: '',
-        title: 'Some title here 7',
-        excerpt: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores, perspiciatis?'
-    },
-    {
-        id: 8,
-        date: '2015-04-23T18:25:43.511Z',
-        content: '',
-        title: 'Some title here 8',
-        excerpt: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores, perspiciatis?'
-    },
-    {
-        id: 9,
-        date: '2012-04-23T18:25:43.511Z',
-        content: '',
-        title: 'Some title here 9',
-        excerpt: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores, perspiciatis?'
-    },
-    {
-        id: 10,
-        date: '2011-04-23T18:25:43.511Z',
-        content: '',
-        title: 'Some title here 10',
-        excerpt: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores, perspiciatis?'
-    },
-    {
-        id: 11,
-        date: '1998-04-23T18:25:43.511Z',
-        content: '',
-        title: 'Some title here 11',
-        excerpt: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores, perspiciatis?'
-    }
-];
+MongoClient = require('mongodb').MongoClient;
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "http://localhost:8080");
@@ -89,5 +9,21 @@ app.use((req, res, next) => {
     next();
 });
 
+const uri = "mongodb://127.0.0.1:27017";
+
+app.get('/events', (req, res) => {
+    MongoClient.connect(uri, {useUnifiedTopology: true}, (err, client) => {
+        if(err) {
+            console.log(err)
+        }
+
+        let db = client.db('ttl-project');
+        let collection = db.collection('events');
+
+        collection.find({}).toArray((err, items) => {
+            res.json(items);
+        })
+    });
+});
+
 app.listen(port);
-app.get('/events', (req, res) => res.json(testData));
