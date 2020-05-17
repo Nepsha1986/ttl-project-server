fed5179e332b797236bc53da4b770c8fde544efe
Oleksandr Nepsha <oleksandr.nepsha@cashstar.com>
2020-05-17 19:47:56 +0300

    Added hashed password.

--- api/user/user.controller.js
+++ api/user/user.controller.js
@@ -1,14 +1,18 @@
 const User = require('./user.dao');
+const bcrypt = require('bcrypt');
 
 exports.createUser = (req, res, next) => {
-    const userData = {
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
-    };
+    bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+            const userData = {
+                username: req.body.username,
+                email: req.body.email,
+                password: hash
+            };
 
-    User.create(userData, (err, user) => {
-        if (err) return next(err);
-        res.json(user);
-    });
+            User.create(userData, (err, user) => {
+                if (err) return next(err);
+                res.json(user);
+            });
+        });
 };
