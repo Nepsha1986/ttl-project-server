5bf4ab783ad2b3e36adcb043afc887f3c448facb
Oleksandr Nepsha <oleksandr.nepsha@cashstar.com>
2020-08-01 19:08:47 +0300

    Fix in user controller.

--- api/user/user.controller.js
+++ api/user/user.controller.js
@@ -2,6 +2,7 @@ const {User} = require('./user.dao');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const UserSchema = require('./user.model');
 
 exports.getUser = (req, res, next) => {
     const token = req.headers["x-access-token"] || req.headers["authorization"];
@@ -11,8 +12,15 @@ exports.getUser = (req, res, next) => {
     try {
         //if can verify the token, set req.user and pass to next middleware
         req.user = jwt.verify(token, config.get("private_key"));
-        console.log(req.user);
-        next();
+
+        User.findById(req.user._id, (err, data) => {
+            if(err) res.status(400).send("Authentication failed. Can not find user.");
+            req.user = {
+                username: data.username,
+                email: data.email
+            };
+            next();
+        });
     } catch (ex) {
         //if invalid token
         res.status(400).send("Invalid token.");
